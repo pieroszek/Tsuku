98c48776175b0c08dcbdefea63b19858a428875d
pieroszek <128466050+pieroszek@users.noreply.github.com>
2024-11-25 22:56:37 +0100

    v 4.1.23

--- tsuku.py
+++ tsuku.py
@@ -4,12 +4,64 @@ import sys
 import os
 
 Args = []
-Items = ["jam", "paint"]
+Items = ["empty"]
 
 createVar = "c"
 remVar = "r"
 listVar = "l"
 
+
+def getFile():
+    data = ""
+    try: 
+        with open("data.txt", 'r') as file :
+            data = file.readline()
+            data.strip()
+            return data
+    except FileExistsError or FileNotFoundError:
+        print("no data file, created one")
+        with open("data.txt", 'x') as file:
+            data = file.readlines()
+            return data
+
+def readFile(data):
+
+    try:
+    #if True:
+        Items.clear()
+        i = 0
+        item = ""
+        while i < len(data):
+            if data[i] != '|':
+                item = item + data[i]
+            elif data[i] == '|':
+                Items.append(item)
+                item = ""
+            i = i + 1
+    except:
+       print("parser brok")
+
+def clearFile():
+    try:
+        with open("data.txt", 'w') as file:
+            pass
+    except:
+        print("ckear file brok")
+
+
+def writeFile():
+    strnToAdd = ""    
+    try:
+        with open("data.txt", 'w') as file:
+            for x in Items:
+                strnToAdd = strnToAdd + x + '|'
+            file.write(strnToAdd)
+    except FileExistsError:
+        print("file no existe")
+    except:
+        print("write error")
+
+####
 def parseArgs():
     i = 0
     for x in sys.argv:
@@ -44,7 +96,8 @@ def create():
         else:
             newItem = Args[1]
         Items.append(newItem)
-        showItems()
+
+        
 
 def remove():
     print("**curr List**")
@@ -61,12 +114,15 @@ def remove():
         for x in Items:
             if x == Args[1]:
                 Items.remove(x)
-    print("**new List**")
-    showItems()
+
+
 
 def list():
     print("**List**")
+    data = getFile()
+    readFile(data)
     showItems()
+        
 
 def argsToItems():
 
@@ -74,14 +130,35 @@ def argsToItems():
         Items.append(arg)
 
 def main():
+    print(getFile())
     parseArgs()
     match Args[0] :
         case "c":
+            data = getFile()
+            clearFile()
+            readFile(data)
             create()
+            writeFile()
+            showItems()
+            Items.clear()
         case "r":
+            data = getFile()
+            clearFile()
+            readFile(data)
             remove()
+            writeFile()
+            print("**new list**")
+            showItems()
+            Items.clear()
         case "l":
-            list()
+            data = getFile()
+           
+            readFile(data)
+            print("**list**")
+            showItems()
+
+            Items.clear()
+            #list()
         case _:
             print("try c, r or l")
 
